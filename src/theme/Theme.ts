77921f8cea2cc4777c66e634ebc1d3937d37b8f2
Viktoriia <rudneva15v@gmail.com>
2023-01-05 13:43:14 +0100

    add object fontSide

---
+++ src/theme/Theme.ts
@@ -0,0 +1,12 @@
+export const Theme = {
+    font1: 'Josefin Sans',
+    font2: 'Cormorant Infant',
+    fontSide15: '15px',
+    fontSide20: '20px',
+    fontSide22: '22px',
+    fontSide25: '25px',
+    fontSide40: '40px',
+    fontSide60: '60px',
+    fontSide30: '30px',
+    fontSide100: '100px',
+} 
