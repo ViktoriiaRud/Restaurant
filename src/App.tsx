3d295af2e7934b9deb5a40b22512658c41ee41b0
Viktoriia <rudneva15v@gmail.com>
2023-01-05 22:55:21 +0100

    start all  components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HeaderRestaurant from './components/headerRestaurant/HeaderRestaurant';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <HeaderRestaurant />
     </div>
   );
 }
---
+++ src/components/headerRestaurant/HeaderRestaurant.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const HeaderRestaurant = () => {
+    return <>
+        <h1>Welcome to Restaurant</h1>
+    </>
+
+
+}
+
+
+export default HeaderRestaurant;
